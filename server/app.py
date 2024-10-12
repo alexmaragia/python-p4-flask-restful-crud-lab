c7511dac6fdd4f5716552c7cfb8675a92ed4a03a
alexmaragia <maragialex@gmail.com>
2024-10-12 23:57:29 +0300

    python-p4-flask-restful-crud-lab

--- server/app.py
+++ server/app.py
@@ -16,9 +16,7 @@ db.init_app(app)
 
 api = Api(app)
 
-
 class Plants(Resource):
-
     def get(self):
         plants = [plant.to_dict() for plant in Plant.query.all()]
         return make_response(jsonify(plants), 200)
@@ -37,19 +35,40 @@ class Plants(Resource):
 
         return make_response(new_plant.to_dict(), 201)
 
-
 api.add_resource(Plants, '/plants')
 
-
 class PlantByID(Resource):
-
     def get(self, id):
-        plant = Plant.query.filter_by(id=id).first().to_dict()
-        return make_response(jsonify(plant), 200)
+        plant = Plant.query.filter_by(id=id).first()
+        if plant:
+            return make_response(jsonify(plant.to_dict()), 200)
+        return make_response(jsonify({"error": "Plant not found"}), 404)
 
+    def patch(self, id):
+        plant = Plant.query.filter_by(id=id).first()
+        if not plant:
+            return make_response(jsonify({"error": "Plant not found"}), 404)
+
+        data = request.get_json()
+
+        for attr in data:
+            setattr(plant, attr, data[attr])
+
+        db.session.commit()
+
+        return make_response(jsonify(plant.to_dict()), 200)
+
+    def delete(self, id):
+        plant = Plant.query.filter_by(id=id).first()
+        if not plant:
+            return make_response(jsonify({"error": "Plant not found"}), 404)
+
+        db.session.delete(plant)
+        db.session.commit()
+
+        return make_response('', 204)
 
 api.add_resource(PlantByID, '/plants/<int:id>')
-
 
 if __name__ == '__main__':
     app.run(port=5555, debug=True)
